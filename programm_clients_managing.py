098f9f3885afd53f095893554f3fd2ee460c49fb
Liaksej <tarusov.liaksej@gmail.com>
2022-08-15 22:59:50 +0200

    Cleaning code and optimizing wrap function

--- programm_clients_managing.py
+++ programm_clients_managing.py
@@ -48,17 +48,19 @@ def create_db():
 def insert_new_client(name, surname, email, **phones):
     with conn.cursor() as cur:
         cur.execute("""
-                INSERT INTO clients(name, surname, email) VALUES(%s, %s, %s);
+                INSERT INTO clients(name, surname, email) 
+                VALUES(%s, %s, %s);
                 """, (name, surname, email))
         conn.commit()
         cur.execute("""
                 SELECT client_id FROM clients
-                WHERE email=%s;
+                 WHERE email=%s;
                 """, (email,))
         clien_id_for_insert_to_phones_tab = cur.fetchone()[0]
         for item in phones['phones']:
             cur.execute("""
-            INSERT INTO phones(phone_number, client_id) VALUES(%s, %s);
+            INSERT INTO phones(phone_number, client_id) 
+            VALUES(%s, %s);
             """, (item, clien_id_for_insert_to_phones_tab))
         conn.commit()
 
@@ -67,14 +69,16 @@ def insert_new_client(name, surname, email, **phones):
 def insert_phone_number_for_existing_client(email, phones):
     with conn.cursor() as cur:
         cur.execute("""
-                SELECT client_id FROM clients
-                WHERE email=%s;
+                SELECT client_id 
+                  FROM clients
+                 WHERE email=%s;
                 """, (email,))
         clien_id_for_insert_to_phones_tab = cur.fetchone()[0]
         for phone in phones:
             cur.execute("""
-            INSERT INTO phones(phone_number, client_id) VALUES(%s, %s);
-            """, (phone, clien_id_for_insert_to_phones_tab))
+                    INSERT INTO phones(phone_number, client_id) 
+                    VALUES(%s, %s);
+                    """, (phone, clien_id_for_insert_to_phones_tab))
         conn.commit()
 
 
@@ -82,56 +86,59 @@ def insert_phone_number_for_existing_client(email, phones):
 def update_data_client(existing_email, **data):
     with conn.cursor() as cur:
         cur.execute("""
-                SELECT client_id FROM clients
-                WHERE email=%s;
+                SELECT client_id 
+                  FROM clients
+                 WHERE email=%s;
                 """, (existing_email,))
         clien_id_for_insert_to_phones_tab = cur.fetchone()[0]
         for item in data:
             if item == 'name':
                 cur.execute("""
-                    UPDATE clients
-                    SET name = %s
-                    WHERE client_id = %s;
+                        UPDATE clients
+                           SET name = %s
+                         WHERE client_id = %s;
                     """, (data['name'], clien_id_for_insert_to_phones_tab))
                 conn.commit()
             elif item == 'surname':
                 cur.execute("""
-                    UPDATE clients
-                    SET surname = %s
-                    WHERE client_id = %s;
+                        UPDATE clients
+                           SET surname = %s
+                         WHERE client_id = %s;
                     """, (data['surname'], clien_id_for_insert_to_phones_tab))
                 conn.commit()
             elif item == 'new_email':
                 cur.execute("""
                        UPDATE clients
-                       SET email = %s
-                       WHERE client_id = %s;
+                          SET email = %s
+                        WHERE client_id = %s;
                        """, (data['new_email'], clien_id_for_insert_to_phones_tab))
                 conn.commit()
             elif item == 'phones':
                 cur.execute("""
-                        SELECT count(client_id) FROM phones
-                        WHERE client_id=%s;
+                        SELECT count(client_id) 
+                          FROM phones
+                         WHERE client_id=%s;
                         """, (clien_id_for_insert_to_phones_tab,))
                 cuantity_of_phones = cur.fetchone()[0]
                 if cuantity_of_phones and len(data['phones']) == 1:
                     for phone in data['phones']:
                         cur.execute("""
                         UPDATE phones
-                        SET phone_number = %s
-                        WHERE client_id = %s;
+                           SET phone_number = %s
+                         WHERE client_id = %s;
                         """, (phone, clien_id_for_insert_to_phones_tab))
                     conn.commit()
                 elif cuantity_of_phones == 0:
                     for phone in data['phones']:
                         cur.execute("""
-                        INSERT INTO phones(phone_number, client_id) VALUES(%s, %s);
+                        INSERT INTO phones(phone_number, client_id) 
+                        VALUES(%s, %s);
                         """, (phone, clien_id_for_insert_to_phones_tab))
                     conn.commit()
                 elif cuantity_of_phones == 1 and len(data['phones']) > 1:
                     cur.execute("""
                             DELETE FROM phones
-                            WHERE client_id = %s;
+                             WHERE client_id = %s;
                     """, (clien_id_for_insert_to_phones_tab,))
                     conn.commit()
                     for phone in data['phones']:
@@ -142,12 +149,13 @@ def update_data_client(existing_email, **data):
                 elif cuantity_of_phones > 1:
                     cur.execute("""
                             DELETE FROM phones
-                            WHERE client_id = %s;
+                             WHERE client_id = %s;
                     """, (clien_id_for_insert_to_phones_tab,))
                     conn.commit()
                     for phone in data['phones']:
                         cur.execute("""
-                            INSERT INTO phones(phone_number, client_id) VALUES(%s, %s);
+                            INSERT INTO phones(phone_number, client_id) 
+                            VALUES(%s, %s);
                             """, (phone, clien_id_for_insert_to_phones_tab))
                     conn.commit()
 
@@ -156,13 +164,15 @@ def update_data_client(existing_email, **data):
 def delete_clients_phones(email):
     with conn.cursor() as cur:
         cur.execute("""
-                SELECT client_id FROM clients
-                WHERE email=%s;
+                SELECT client_id 
+                  FROM clients
+                 WHERE email=%s;
                 """, (email,))
         clien_id_for_insert_to_phones_tab = cur.fetchone()[0]
         cur.execute("""
-                SELECT count(client_id) FROM phones
-                WHERE client_id=%s;
+                SELECT count(client_id) 
+                  FROM phones
+                 WHERE client_id=%s;
                 """, (clien_id_for_insert_to_phones_tab,))
         cuantity_of_phones = cur.fetchone()[0]
         if cuantity_of_phones > 0:
@@ -177,8 +187,9 @@ def delete_clients_phones(email):
 def delete_client(email):
     with conn.cursor() as cur:
         cur.execute("""
-                SELECT client_id FROM clients
-                WHERE email=%s;
+                SELECT client_id 
+                  FROM clients
+                 WHERE email=%s;
                 """, (email,))
         clien_id_for_insert_to_phones_tab = cur.fetchone()[0]
         cur.execute("""
@@ -198,33 +209,37 @@ def find_client(**kwargs):
         if item == 'name':
             with conn.cursor() as cur:
                 cur.execute("""
-                        SELECT name, surname, email, phone_number FROM clients c
-                        LEFT JOIN phones p ON c.client_id = p.client_id
-                        WHERE name=%s;
+                        SELECT name, surname, email, phone_number 
+                          FROM clients c
+                     LEFT JOIN phones p ON c.client_id = p.client_id
+                         WHERE name=%s;
                         """, (kwargs['name'],))
                 show_result = cur.fetchall()
         elif item == 'surname':
             with conn.cursor() as cur:
                 cur.execute("""
-                        SELECT name, surname, email, phone_number FROM clients c
-                        LEFT JOIN phones p ON c.client_id = p.client_id
-                        WHERE surname=%s;
+                        SELECT name, surname, email, phone_number 
+                          FROM clients c
+                     LEFT JOIN phones p ON c.client_id = p.client_id
+                         WHERE surname=%s;
                         """, (kwargs['surname'],))
                 show_result = cur.fetchall()
         elif item == 'email':
             with conn.cursor() as cur:
                 cur.execute("""
-                        SELECT name, surname, email, phone_number FROM clients c
-                        LEFT JOIN phones p ON c.client_id = p.client_id
-                        WHERE email=%s;
+                        SELECT name, surname, email, phone_number 
+                          FROM clients c
+                     LEFT JOIN phones p ON c.client_id = p.client_id
+                         WHERE email=%s;
                         """, (kwargs['email'],))
                 show_result = cur.fetchall()
         elif item == 'phones':
             with conn.cursor() as cur:
                 cur.execute("""
-                        SELECT name, surname, email, phone_number FROM clients c
-                        JOIN phones p ON c.client_id = p.client_id
-                        WHERE phone_number=%s;
+                        SELECT name, surname, email, phone_number 
+                          FROM clients c
+                          JOIN phones p ON c.client_id = p.client_id
+                         WHERE phone_number=%s;
                         """, (kwargs['phones'],))
                 show_result = cur.fetchall()[0]
         return show_result
